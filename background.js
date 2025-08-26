9f0f65319475b65ce9cb1e51cbb8e0e94f81cdc5
Ali <41494681+Gasimzade09@users.noreply.github.com>
2025-08-26 11:09:55 +0400

    Update background.js

--- background.js
+++ background.js
@@ -1,17 +1,1 @@
-chrome.webRequest.onBeforeRequest.addListener(
-  function (details) {
-    const url = new URL(details.url);
 
-    // Если уже есть tag — ничего не делаем
-    if (url.searchParams.has("tag")) {
-      return {};
-    }
-
-    // Добавляем tag
-    url.searchParams.append("tag", "smartcashba04-20");
-
-    return { redirectUrl: url.toString() };
-  },
-  { urls: ["*://www.amazon.com/*"] },
-  ["blocking"]
-);
